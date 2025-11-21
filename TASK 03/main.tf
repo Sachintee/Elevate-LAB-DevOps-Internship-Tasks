41057e14830ba5de390dba1aff9e9193d14f661c
Sachin <102177619+Sachintee@users.noreply.github.com>
2025-11-21 14:31:56 +0530

    Update main.tf

--- TASK 03/main.tf
+++ TASK 03/main.tf
@@ -9,14 +9,15 @@ terraform {
 
 provider "docker" {}
 
+# Pull nginx image
 resource "docker_image" "nginx_image" {
   name = "nginx:latest"
 }
 
+# Run nginx container
 resource "docker_container" "nginx_container" {
   name  = "nginx-container"
-  image = docker_image.nginx_image.name
-
+  image = docker_image.nginx_image.name   # <-- FIXED LINE
   ports {
     internal = 80
     external = 8080
