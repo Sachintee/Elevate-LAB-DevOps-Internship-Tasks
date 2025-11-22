fcb95a12ae007b29c0115720cf3eebfe9ac85477
Sachin <102177619+Sachintee@users.noreply.github.com>
2025-11-23 00:16:03 +0530

    Update main.tf

--- TASK 03/main.tf
+++ TASK 03/main.tf
@@ -1,14 +1,23 @@
-# This is where the error is fixed
-resource "docker_container" "nginx_container" {
-  name  = "my-terraform-nginx-container"
-  
-  # CORRECT: Reference the image ID from the docker_image resource
-  image = docker_image.nginx_image.image_id 
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+}
 
+provider "docker" {}
+
+resource "docker_image" "nginx_image" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "my-nginx-container"
+  image = docker_image.nginx_image.name
   ports {
     internal = 80
     external = 8080
   }
-  
-  # ... other configuration ...
 }
