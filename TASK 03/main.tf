b1ca92836da1c3b01088a685ed8791548f48e9f3
SachinTeenwal <sachinkumawat54650@gmail.com>
2025-11-23 17:17:44 +0530

    Add main.tf

--- TASK 03/main.tf
+++ TASK 03/main.tf
@@ -1,8 +1,8 @@
-terraform {
+﻿terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0.0"
+      version = ">= 3.0.0"
     }
   }
 }
@@ -14,9 +14,8 @@ resource "docker_image" "nginx_image" {
 }
 
 resource "docker_container" "nginx_container" {
-  name  = "terraform-nginx"
-  image = docker_image.nginx_image.latest
-
+  name  = "my-nginx"
+  image = docker_image.nginx_image.image_id
   ports {
     internal = 80
     external = 8080
